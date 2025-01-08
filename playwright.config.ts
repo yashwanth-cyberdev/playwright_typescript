83c972f97249f7b771a242ece2fb964113713e5f
yashwanth_nagesh <yashwanth.n@qsgsoft.com>
2025-01-08 18:08:10 +0530

    ci: added workflow file

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["html"],
